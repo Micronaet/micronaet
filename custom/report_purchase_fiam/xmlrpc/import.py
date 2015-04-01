0906ab80f2bb318e51a078c20d2ea7f05f37598c
nicothebrush <nicola.riolini@gmail.com>
2015-04-01 12:16:19 +0200

    Add note in report XLS

--- custom/report_purchase_fiam/xmlrpc/import.py
+++ custom/report_purchase_fiam/xmlrpc/import.py
@@ -33,6 +33,16 @@ filename = os.path.join(path, 'product.csv')
 # -----------------------------------------------------------------------------
 # Function:
 # -----------------------------------------------------------------------------
+def clean(valore):
+    res = ""
+    for c in valore:
+        if ord(c) >= 128:
+            res += "#"
+        else: 
+            res += c    
+        
+    return res    
+
 def format_string(valore):
     valore = valore.decode('cp1252')
     valore = valore.encode('utf-8')
@@ -83,76 +93,79 @@ lines = open(filename, 'rb')
 
 i = 0
 for row in lines:
+    row = clean(row)
     line = row.strip().split(separator)
     i += 1 
-    import pdb; pdb.set_trace()
     if not len(line) or len(line) != 23: # jump empty lines
         print "line jumped"
         continue
-    try:
-        default_code = format_string(line[0])
-        name = format_string(line[1])
-        description = format_string(line[2])
-        campaign_qty = format_string(line[3])
-        price_supplier = format_float(line[4])
-        material = format_string(line[5])
-        colour = format_string(line[6])
-        unshield = format_string(line[7])
-        wash = format_string(line[8])
-        L = format_float(line[9])
-        H = format_float(line[10])
-        D = format_float(line[11])
-        diameter = format_string(line[12])
-        H_sit = format_string(line[13])
-        comment = format_string(line[14])
-        weight = format_float(line[15])
-        mounted = format_string(line[16])
-        #L1 = format_float(line[17])
-        #H1 = format_float(line[18])
-        #D1 = format_float(line[19])
-        weight1 = format_float(line[20])
-        colls = format_string(line[21])
-        ean13 = format_string(line[22])
+    default_code = format_string(line[0])
+    name = format_string(line[1])
+    description = format_string(line[2])
+    campaign_qty = format_string(line[3])
+    price_supplier = format_float(line[4])
+    material = format_string(line[5])
+    colour = format_string(line[6])
+    unshield = format_string(line[7])
+    wash = format_string(line[8])
+    L = format_float(line[9])
+    H = format_float(line[10])
+    D = format_float(line[11])
+    diameter = format_string(line[12])
+    H_sit = format_string(line[13])
+    comment = format_string(line[14])
+    weight = format_float(line[15])
+    mounted = format_string(line[16])
+    #L1 = format_float(line[17])
+    #H1 = format_float(line[18])
+    #D1 = format_float(line[19])
+    weight1 = format_float(line[20])
+    colls = format_string(line[21])
+    ean13 = format_string(line[22])
+   
+    data = {
+        'default_code': default_code,
+        #name,
+        #description,
+        #campaign_qty,
+        #price_supplier,
+        #material,
+        'colour': colour,
+        #unshield,
+        #wash,
+        'height': L,
+        'width': H,
+        'length': D,
+        #diameter,
+        #H_sit,
+        #comment,
+        'weight': weight,
+        #mounted,
+        #L1,
+        #H1,
+        #D1,
+        #weight1,
+        #colls,
+        'ean13': ean13,
+        }
        
-        data = {
-            'default_code': default_code,
-            #name,
-            #description,
-            #campaign_qty,
-            #price_supplier,
-            #material,
-            'colour': colour,
-            #unshield,
-            #wash,
-            'height': L,
-            'width': H,
-            'length': D,
-            #diameter,
-            #H_sit,
-            #comment,
-            'weight': weight,
-            #mounted,
-            #L1,
-            #H1,
-            #D1,
-            #weight1,
-            #colls,
-            'ean13': ean13,
-            }
-           
-        product_ids = sock.execute( # search current ref
-            dbname, uid, pwd, 'product.product', 'search', [
-                ('default_code', '=', default_code),])                       
-           
-        if product_ids:
+    product_ids = sock.execute( # search current ref
+        dbname, uid, pwd, 'product.product', 'search', [
+            ('default_code', '=', default_code),])                       
+       
+    if product_ids:
+        try:
             sock.execute( # search current ref
                 dbname, uid, pwd, 'product.product', 'write', 
                 product_ids, data)                   
-        else:
-            print "Code not found", default_code
-            #product_id = sock.execute(
-            #    dbname, uid, pwd, 'product.product', 'create', data)                   
-        print "Line", i
-    except:
-        print "Errore importando, record saltato", sys.exc_info()    
-        continue
+        except:
+            print "Riga", i, "Error:", row#, data
+            continue        
+    else:
+        print "Riga", i, "Code not found", default_code
+        #product_id = sock.execute(
+        #    dbname, uid, pwd, 'product.product', 'create', data)                   
+    print "Riga", i
+    #except:
+    #    print "Errore importando, record saltato", sys.exc_info()    
+    #    continue
