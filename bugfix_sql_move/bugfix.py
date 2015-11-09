a48fb92d3fba50d235aef3e75bf1f6dddbeb7822
nicothebrush <nicola.riolini@gmail.com>
2015-11-09 18:03:04 +0100

    Start procedure for save correct ID in destination field (and deactivate - delete partner)

--- bugfix_sql_move/bugfix.py
+++ bugfix_sql_move/bugfix.py
@@ -47,12 +47,60 @@ class sql_move_line(osv.osv):
         ''' Force unification for partner double
             Re assign all movement line
         '''
-        # Save original log partner in movement:        
-        cr.execute( # Update only once:
-            ''' UPDATE sql_move_line
-                SET bugfix_old_id = partner_id 
-                WHERE bugfix_old_id is null;
-            ''')        
+        # Read customer for get convert dictionary:
+        customer_ids = self.search(cr, uid, [
+            ('sql_customer_code', '!=', False),
+            ('sql_import', '=', True),
+            ('type', '=', 'default'),            
+            ], context=context)
+        customer_db = {}
+        for customer in self.browse(cr, uid, customer_ids, 
+                context=context):
+            customer_db[customer.sql_customer_code] = customer.id
+        import pdb; pdb.set_trace()    
+            
+        # Read customer for get convert dictionary:
+        supplier_ids = self.search(cr, uid, [
+            ('sql_supplier_code', '!=', False),
+            ('sql_import', '=', True),
+            ('type', '=', 'default'),            
+            ], context=context)
+        supplier_db = {}
+        for supplier in self.browse(cr, uid, supplier_ids, 
+                context=context):
+            supplier_db[customer.sql_supplier_code] = supplier.id
+        import pdb; pdb.set_trace()    
+            
+        # Read customer for unlink:
+        destination_ids = self.search(cr, uid, [ # TO remove
+            ('sql_destination_code', '!=', False),
+            ('sql_import', '=', True),
+            ('type', '=', 'contact'),
+            ], context=context)
+        import pdb; pdb.set_trace()    
+        for destination in self.browse(cr, uid, destination_ids, 
+                context=context):
+            # Save changed ID:
+            code = destination.sql_destination_code
+            data = {}
+            if code and code in customer_db:
+                data.update({
+                    'name': '[RIMUOVERE] %s' % destination.name,
+                    #'active': False,
+                    'bugfix_id': customer_db[code],
+                    })
+            else:
+                if code and code in supplier_db:
+                    data.update({
+                        'name': '[RIMUOVERE] %s' % destination.name,
+                        #'active': False,
+                        'bugfix_id': supplier_db[code],
+                        })
+            if data:            
+                self.write(cr, uid, destination.id, data, context=context)    
+
+        # Update all lines from destination to client or supplier:
+        # TODO                
         return True        
         
     _columns = {
