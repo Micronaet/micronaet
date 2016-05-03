214fd0b41c53d9c9585825c966af1b9e05437a41
nicothebrush <nicola.riolini@gmail.com>
2016-05-03 11:45:14 +0200

    Add add extra filter to wizard

--- production_line/wizard/print_covered_report.py
+++ production_line/wizard/print_covered_report.py
@@ -25,14 +25,20 @@
 #
 ##############################################################################
 
-from openerp.osv import fields, osv
 import os
+from openerp.osv import fields, osv
+from datetime import datetime, timedelta
+from openerp.tools import (DEFAULT_SERVER_DATE_FORMAT, 
+    DEFAULT_SERVER_DATETIME_FORMAT, 
+    DATETIME_FORMATS_MAP, 
+    float_compare)
+
 
 class sale_order_line_covered_wizard(osv.osv_memory):
     ''' Wizard that assign lavoration to the selected order line
     '''
-    _name = "sale.order.line.covered.wizard"
-    _description = "Open covered report"
+    _name = 'sale.order.line.covered.wizard'
+    _description = 'Open covered report'
 
     # Wizard button:
     def action_open_report(self, cr, uid, ids, context=None):
@@ -46,7 +52,7 @@ class sale_order_line_covered_wizard(osv.osv_memory):
         
         return {
             'type': 'ir.actions.report.xml', 
-            'report_name':'order_covered',        
+            'report_name': 'order_covered',        
             'datas': data,
             }
                
@@ -56,11 +62,15 @@ class sale_order_line_covered_wizard(osv.osv_memory):
             ('order', 'Solo ordini coperti interamente'),            
             ('all', 'Tutti le righe ordine'),
         ],'type', select=True, required=True),
-        }           
+        'from_date': fields.date('From'),
+        'to_date': fields.date('To'),
+        'partner_id': fields.many2one(
+            'res.partner', 'Partner'), 
+        }
     
     _defaults = {
         'type': lambda *a: 'all',
+        'to_date': lambda *a: (datetime.now() + timedelta(days=7)).strftime(
+            DEFAULT_SERVER_DATE_FORMAT)
         }
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
-
-
