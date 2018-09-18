bbad7f48ffb43941f087f2247d2363cc22167238
nicothebrush <nicola.riolini@gmail.com>
2018-09-18 09:59:39 +0200

    [ADD] production_pedimento for manage duty code

---
+++ production_pedimento/pedimento.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+# ODOO (ex OpenERP) 
+# Open Source Management Solution
+# Copyright (C) 2001-2015 Micronaet S.r.l. (<https://micronaet.com>)
+# Developer: Nicola Riolini @thebrush (<https://it.linkedin.com/in/thebrush>)
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+# See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+import os
+import sys
+import logging
+import openerp
+import openerp.netsvc as netsvc
+import openerp.addons.decimal_precision as dp
+from openerp.osv import fields, osv, expression, orm
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+from openerp import SUPERUSER_ID
+from openerp import tools
+from openerp.tools.translate import _
+from openerp.tools.float_utils import float_round as round
+from openerp.tools import (DEFAULT_SERVER_DATE_FORMAT, 
+    DEFAULT_SERVER_DATETIME_FORMAT, 
+    DATETIME_FORMATS_MAP, 
+    float_compare)
+
+
+_logger = logging.getLogger(__name__)
+
+class ProductProductPedimento(orm.Model):
+    """ Model name: ProductProductPedimento
+    """
+    
+    _name = 'product.product.pedimento'
+    _description = 'Pedimento'
+    _rec_name = 'name'
+    _order = 'name'
+    
+    _columns = {
+        'name': fields.char('Name', size=64, required=True),
+        'product_id': fields.many2one('product.product', 'Product'),
+        }
+        
+class ProductProduct(orm.Model):
+    """ Model name: ProductProduct
+    """
+    
+    _inherit = 'product.product'
+    
+    _columns = {
+        'pedimento_ids': fields.one2many(
+            'product.product.pedimento', 'product_id', 'Pedimento'),
+        }
+
+class MrpProductionMaterial(orm.Model):
+    """ Model name: Material pedimento
+    """
+    
+    _inherit = 'mrp.production.material'
+    
+    _columns = {
+        'pedimento_id': fields.many2one(
+            'product.product.pedimento', 'Pedimento'),
+        }
+
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
