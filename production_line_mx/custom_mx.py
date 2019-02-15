2338e133a76995c3774889b4bbd95804a2b0bd73
nicothebrush <nicola.riolini@gmail.com>
2019-02-15 17:09:54 +0100

    generic bugfix (fast message)

--- production_line_mx/custom_mx.py
+++ production_line_mx/custom_mx.py
@@ -587,7 +587,11 @@ class MrpProductionWorkcenterLineExtra(osv.osv):
         # Update database with log files:
         _logger.info('Update log info in database:')             
         for wc_id in wc_db:
-            self.write(cr, uid, [wc_id], wc_db[wc_id], context=context)
+            try:
+                self.write(cr, uid, [wc_id], wc_db[wc_id], context=context)
+            except:
+                _logger.info('Workcenter ID %s no more present!' % wc_id)
+                    
             
         # History file read (after modiy database):
         _logger.info('History file readed:')
