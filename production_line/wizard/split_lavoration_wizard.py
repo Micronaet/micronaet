922902a38558e0d9e570126dcf1a1ba214391b9e
nicothebrush <nicola.riolini@gmail.com>
2019-06-18 12:14:06 +0200

    generic bugfix (fast message)

--- production_line/wizard/split_lavoration_wizard.py
+++ production_line/wizard/split_lavoration_wizard.py
@@ -51,6 +51,7 @@ class mrp_production_split_wizard(osv.osv_memory):
         working_hour_day = lavoration_proxy.workcenter_id.hour_daily_work if lavoration_proxy.workcenter_id else 16 #(default)
         if lavoration_proxy and split_daily:        
             current_start = datetime.strptime(lavoration_proxy.real_date_planned[:10], "%Y-%m-%d")
+            import pdb; pdb.set_trace()
             daily_cycle = int(working_hour_day // lavoration_proxy.single_cycle_duration)
             if not daily_cycle:
                 raise osv.except_osv(
@@ -175,6 +176,7 @@ class mrp_production_split_wizard(osv.osv_memory):
             current_date = current_start + relativedelta(days = 1) # jump first day
             
             working_hour_day = lavoration_proxy.workcenter_id.hour_daily_work if lavoration_proxy.workcenter_id else 16 #(default)
+            import pdb; pdb.set_trace()
             daily_cycle = int(working_hour_day // lavoration_proxy.single_cycle_duration)   # cycle that I can do per day
             if not daily_cycle:
                 raise osv.except_osv(
