0d324d021a8ad756b494b02ad02f24c51b9cfba1
nicothebrush <nicola.riolini@gmail.com>
2020-12-31 08:51:18 +0100

    pep 8 normalitazion

--- production_pedimento/pedimento.py
+++ production_pedimento/pedimento.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 ###############################################################################
 #
-# ODOO (ex OpenERP) 
+# ODOO (ex OpenERP)
 # Open Source Management Solution
 # Copyright (C) 2001-2015 Micronaet S.r.l. (<https://micronaet.com>)
 # Developer: Nicola Riolini @thebrush (<https://it.linkedin.com/in/thebrush>)
@@ -13,7 +13,7 @@
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 # See the GNU Affero General Public License for more details.
 #
 # You should have received a copy of the GNU Affero General Public License
@@ -35,9 +35,9 @@ from openerp import tools
 from openerp.tools.translate import _
 from openerp.tools.float_utils import float_round as round
 from openerp.tools import (
-    DEFAULT_SERVER_DATE_FORMAT, 
-    DEFAULT_SERVER_DATETIME_FORMAT, 
-    DATETIME_FORMATS_MAP, 
+    DEFAULT_SERVER_DATE_FORMAT,
+    DEFAULT_SERVER_DATETIME_FORMAT,
+    DATETIME_FORMATS_MAP,
     float_compare)
 
 
@@ -46,56 +46,56 @@ _logger = logging.getLogger(__name__)
 class ProductProductPedimento(orm.Model):
     """ Model name: ProductProductPedimento
     """
-    
+
     _name = 'product.product.pedimento'
     _description = 'Pedimento'
     _rec_name = 'name'
     _order = 'name'
-    
+
     def name_get(self, cr, uid, ids, context=None):
         """ Return a list of tupples contains id, name.
             result format : {[(id, name), (id, name), ...]}
-            
+
             @param cr: cursor to database
             @param uid: id of current user
             @param ids: list of ids for which name should be read
             @param context: context arguments, like lang, time zone
-            
+
             @return: returns a list of tupples contains id, name
-        """        
+        """
         if isinstance(ids, (list, tuple)) and not len(ids):
             return []
         if isinstance(ids, (long, int)):
-            ids = [ids]            
+            ids = [ids]
         res = []
         for record in self.browse(cr, uid, ids, context=context):
             res.append((
-                record.id, 
+                record.id,
                 _(u'%s [Q. %s]') % (record.name, record.product_qty),
                 ))
         return res
-    
+
     _columns = {
         'name': fields.char('Name', size=64, required=True),
-        'code': fields.char('Code', size=64, 
+        'code': fields.char('Code', size=64,
             help='Same as name but without blanks'),
         'product_id': fields.many2one('product.product', 'Product'),
         'product_qty': fields.float('Qty', digits=(16, 3)),
         'standard_price': fields.float('Cost', digits=(16, 3)),
         }
-        
+
 class ProductProduct(orm.Model):
     """ Model name: ProductProduct
     """
-    
+
     _inherit = 'product.product'
-    
+
     _columns = {
         'product_type': fields.selection([
             ('MP', 'Raw material'),
             ('PT', 'Final product'),
             ], 'Product type'),
-            
+
         'product_mode': fields.selection([
             ('lot', 'Lot'),
             ('pedimento', 'Pedimento'),
@@ -109,4 +109,3 @@ class ProductProduct(orm.Model):
         # Default value:
         'product_mode': lambda *x: 'unit',
         }
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
