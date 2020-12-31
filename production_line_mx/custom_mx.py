5d601d5a94b19ebf391fa107aca08e34631f3baf
nicothebrush <nicola.riolini@gmail.com>
2020-12-31 09:07:35 +0100

    generic bugfix (fast message)

--- production_line_mx/custom_mx.py
+++ production_line_mx/custom_mx.py
@@ -506,7 +506,7 @@ class product_product_extra(osv.osv):
         for product_id in total:
             product_qty, last_cost, control = total[product_id]
             if control == 'lotandpediment':
-                control = 'lot'
+                control = 'pediment'
 
             # -----------------------------------------------------------------
             # Update product data:
