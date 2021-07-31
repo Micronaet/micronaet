fc6d2d1f0ccc91be3a171ad7effe9edf38221d4f
nicothebrush <nicola.riolini@gmail.com>
2021-07-31 11:43:03 +0200

    generic bugfix (fast message)

--- product_status_excel/wizard/status_wizard.py
+++ product_status_excel/wizard/status_wizard.py
@@ -193,11 +193,15 @@ class ProductExtractProductXlsWizard(orm.TransientModel):
                 ], format_title)
 
             excel_pool.column_width(ws_name, [
-                1, 5, 5,
+                1, 1, 1,
+                5, 5,
                 10, 40, 20, 10, 12, 30,
-                10, 10, 10, 10])
+                10, 10, 10, 10, 15])
             header = [
                 'ID',
+                'Obsol. (orig.)',
+                'Escludi (orig.)',
+
                 'Obsol.',
                 'Escludi',
 
@@ -218,7 +222,7 @@ class ProductExtractProductXlsWizard(orm.TransientModel):
             row += 2
             excel_pool.write_xls_line(ws_name, row, header, format_header)
             excel_pool.autofilter(ws_name, row, 0, row, len(header) - 1)
-            excel_pool.freeze_panes(ws_name, row + 1, 4)
+            excel_pool.freeze_panes(ws_name, row + 1, 6)
             excel_pool.column_hidden(ws_name, [0])
 
             for product in sorted(product_pool.browse(
@@ -240,7 +244,12 @@ class ProductExtractProductXlsWizard(orm.TransientModel):
                     state = 'Non presente'
 
                 excel_pool.write_xls_line(ws_name, row, [
+                    # Hidden:
                     product.id,
+                    'X' if product.stock_obsolete else '',
+                    'X' if product.not_in_status else '',
+
+                    # Showed
                     'X' if product.stock_obsolete else '',
                     'X' if product.not_in_status else '',
                     product.default_code,
