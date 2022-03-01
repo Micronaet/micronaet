0f4535f0e3b28486d8f6bb67b9ef80512d9f143e
nicothebrush <nicola.riolini@gmail.com>
2022-03-01 15:03:06 +0100

    generic bugfix (fast message)

--- production_lot_status/lot.py
+++ production_lot_status/lot.py
@@ -24,9 +24,9 @@ import logging
 from openerp.osv import osv, orm, fields
 from datetime import datetime, timedelta
 from openerp.tools import (
-    DEFAULT_SERVER_DATE_FORMAT, 
-    DEFAULT_SERVER_DATETIME_FORMAT, 
-    DATETIME_FORMATS_MAP, 
+    DEFAULT_SERVER_DATE_FORMAT,
+    DEFAULT_SERVER_DATETIME_FORMAT,
+    DATETIME_FORMATS_MAP,
     float_compare,
     )
 import openerp.addons.decimal_precision as dp
@@ -36,26 +36,27 @@ from openerp.tools.translate import _
 _logger = logging.getLogger(__name__)
 
 class stock_production_lot_accounting(orm.Model):
-    ''' Add extra field for manage status of lot from accounting
-    '''
+    """ Add extra field for manage status of lot from accounting
+    """
     _name = 'stock.production.lot'
     _inherit = 'stock.production.lot'
-    
+
     # -----------------
     # Scheduled action:
     # -----------------
-    def scheduled_import_lot_quantity(self, cr, uid, path, filename, 
-            package=True, context=None):        
-        ''' Scheduled function for import status lot from accounting
+    def scheduled_import_lot_quantity(
+            self, cr, uid, path, filename,
+            package=True, context=None):
+        """ Scheduled function for import status lot from accounting
             self: this instance
             cr: cursor
-            uid: user id            
+            uid: user id
             path: folder path of csv file
             filename: csv file name
             package: manage package (default False)
-        '''
-        # TODO remove importation!!!!!!!
-        
+        """
+        # todo remove importation!!!!!!!
+
         if context is None:
             context = {}
         separator = ";"
@@ -66,17 +67,17 @@ class stock_production_lot_accounting(orm.Model):
         except:
             _logger.error("Error accessing file: %s" % file_csv)
             return False
-            
+
         product_pool = self.pool.get("product.product")
         package_pool = self.pool.get("product.ul")
-        
+
         i = 0
         lot_modify_ids = []
         for line in f:
             try:
                 i += 1
                 line_csv = line.strip().split(separator)
-                
+
                 # Fields:
                 product_code = line_csv[0].strip()
                 lot_code = line_csv[1].strip()
@@ -88,13 +89,13 @@ class stock_production_lot_accounting(orm.Model):
                     lot_code = "%06d" % (int(lot_code), )
                     anomaly = False
                 except:
-                    anomaly = True    
+                    anomaly = True
 
                 if package:
                     package_ids = package_pool.search(cr, uid, [
                         ('code', '=', package_code)], context=context)
                     if package_ids:
-                        package_id = package_ids[0]    
+                        package_id = package_ids[0]
                     else: # fast create a package
                         package_id = package_pool.create(cr, uid, {
                             'code': package_code,
@@ -103,9 +104,9 @@ class stock_production_lot_accounting(orm.Model):
                             }, context=context)
                     name = "%s-%s" % (lot_code, package_code)
                 else:
-                    package_id = False        
+                    package_id = False
                     name = lot_code
-                            
+
                 # Get ref by ID of code read
                 product_ids = product_pool.search(cr, uid, [
                     ('default_code', '=', product_code)], context=context)
@@ -114,7 +115,7 @@ class stock_production_lot_accounting(orm.Model):
                             i, product_code))
                     continue
 
-                # Search lot            
+                # Search lot
                 lot_ids = self.search(cr, uid, [
                     ('name', '=', name)], context=context)
                 data = {
@@ -122,47 +123,47 @@ class stock_production_lot_accounting(orm.Model):
                     'product_id': product_ids[0],
                     'package_id': package_id,
                     'stock_available_accounting': stock_available_accounting,
-                    'ref': accounting_ref, # ID in accounting                    
+                    'ref': accounting_ref, # ID in accounting
                     'anomaly': anomaly,
                     }
 
                 if lot_ids:
                     lot_id = lot_ids[0]
-                    self.write(cr, uid, lot_id, data, context=context)    
+                    self.write(cr, uid, lot_id, data, context=context)
                 else:
-                    lot_id = self.create(cr, uid, data, context=context)                    
-                lot_modify_ids.append(lot_id)    
-            except:            
-                _logger.error("Umanaged error: %s" % (sys.exc_info(), ))        
-            
+                    lot_id = self.create(cr, uid, data, context=context)
+                lot_modify_ids.append(lot_id)
+            except:
+                _logger.error("Umanaged error: %s" % (sys.exc_info(), ))
+
         # reset lot not present:
         no_stock_ids = self.search(cr, uid, [('id', 'not in', lot_modify_ids)], context=context)
         self.write(cr, uid, no_stock_ids, {
             'stock_available_accounting': 0.0,
             }, context=context)
         return True
-    
+
     _columns = {
-        'package_id': fields.many2one('product.ul', 'Package', 
+        'package_id': fields.many2one('product.ul', 'Package',
             help='Package used for package (for this lot)'),
         # TODO remove
         'stock_available_accounting': fields.float(
-            'Stock availability', digits=(16, 2)),    
-        'accounting_ref': fields.char('Accounting ref', size=12, 
+            'Stock availability', digits=(16, 2)),
+        'accounting_ref': fields.char('Accounting ref', size=12,
             help="ID lot in account program"),
-        'anomaly': fields.boolean('Anomaly', required=False),    
-    }    
+        'anomaly': fields.boolean('Anomaly', required=False),
+    }
 
 class product_product_lot_accounting(orm.Model):
-    ''' Add extra field for manage status of product / lot from accounting
-    '''
+    """ Add extra field for manage status of product / lot from accounting
+    """
     _name = 'product.product'
     _inherit = 'product.product'
-    
+
     _columns = {
-        'accounting_lot_ids': fields.one2many('stock.production.lot', 
+        'accounting_lot_ids': fields.one2many('stock.production.lot',
             'product_id', 'Lots status'),
-        'package_ref': fields.char('Package ref', size=12, 
-            help="Reference for import package, used to find product"),    
+        'package_ref': fields.char('Package ref', size=12,
+            help="Reference for import package, used to find product"),
     }
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
