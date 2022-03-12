b2d1c08b1deb5d45c1b48c2333441fffbd894ffc
nicothebrush <nicola.riolini@gmail.com>
2022-03-12 12:31:38 +0100

    generic bugfix (fast message)

--- production_line/daily_status_check.py
+++ production_line/daily_status_check.py
@@ -573,12 +573,12 @@ class MrpProductionDailyReport(orm.Model):
             col = line_gap + position
             from_cell = excel_pool.rowcol_to_cell(row+2, col)
             to_cell = excel_pool.rowcol_to_cell(row+total_row, col)
-            formula = "=SUBTOTAL(9;%s:%s)" % (from_cell, to_cell)
+            formula = "SUBTOTAL(9;%s:%s)" % (from_cell, to_cell)
             excel_pool.write_formula(
                 ws_name,
                 row, col, formula,
                 excel_format['']['number'],
-                '', # total_line[position],
+                total_line[position],
             )
 
         excel_pool.autofilter(ws_name, 2, 0, 2, max_col)
