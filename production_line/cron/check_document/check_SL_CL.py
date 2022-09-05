e08bb5ee8cdace6a26244bd7e75383c45e9e9742
nicothebrush <nicola.riolini@gmail.com>
2022-09-05 09:48:58 +0200

    add cron action to check CL and SL

---
+++ production_line/cron/check_document/check_SL_CL.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+# ODOO (ex OpenERP)
+# Open Source Management Solution
+# Copyright (C) 2001-2015 Micronaet S.r.l. (<http://www.micronaet.it>)
+# Developer: Nicola Riolini @thebrush (<https://it.linkedin.com/in/thebrush>)
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+import os
+import sys
+import erppeek
+import ConfigParser
+import smtplib
+from datetime import datetime
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email.mime.text import MIMEText
+from email import Encoders
+
+# -----------------------------------------------------------------------------
+# Read configuration parameter:
+# -----------------------------------------------------------------------------
+cfg_file = os.path.expanduser('./openerp.cfg')
+config = ConfigParser.ConfigParser()
+config.read([cfg_file])
+
+# ERP Connection:
+odoo = {
+    'database': config.get('dbaccess', 'dbname'),
+    'user': config.get('dbaccess', 'user'),
+    'password': config.get('dbaccess', 'pwd'),
+    'server': config.get('dbaccess', 'server'),
+    'port': config.get('dbaccess', 'port'),
+    }
+
+
+# -----------------------------------------------------------------------------
+# Connect to ODOO:
+# -----------------------------------------------------------------------------
+odoo = erppeek.Client(
+    'http://%s:%s' % (odoo['server'], odoo['port']),
+    db=odoo['database'],
+    user=odoo['user'],
+    password=odoo['password'],
+    )
+
+company = odoo.model('res.company')
+
+# Launch extract procedure:
+company_id = company.search([])[0]
+company.check_account_document(company_id)
