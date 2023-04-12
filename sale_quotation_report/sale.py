731debab5a57a658ad98531f627cefd660f09edc
nicothebrush <nicola.riolini@gmail.com>
2023-04-12 11:16:38 +0200

    pep 8 normalitazion

--- sale_quotation_report/sale.py
+++ sale_quotation_report/sale.py
@@ -6,7 +6,7 @@
 #    Italian OpenERP Community (<http://www.openerp-italia.com>)
 #
 #    ########################################################################
-#    OpenERP, Open Source Management Solution	
+#    OpenERP, Open Source Management Solution
 #    Copyright (C) 2004-2008 Tiny SPRL (<http://tiny.be>). All Rights Reserved
 #    $Id$
 #
@@ -28,40 +28,47 @@
 from openerp.osv import fields,osv
 from openerp.tools.translate import _
 
+
 class sale_order_override(osv.osv):
-    ''' Change some thing in sale order for use this report
+    """ Change some thing in sale order for use this report
         Add some extra fields for this quotation document
-    '''
+    """
     _name = 'sale.order'
     _inherit = 'sale.order'
-    
-    # Override function that print order
-    def print_quotation(self, cr, uid, ids, context=None):        
-        '''
-        Note: same at the original except for report_name returned
-        This function prints the sales order and mark it as sent, so that we can see more easily the next step of the workflow
-        '''
-        result = super(sale_order_override, self).print_quotation(cr, uid, ids, context=context)
 
-        datas=result.get('datas',{})
-        return {'type': 'ir.actions.report.xml', 'report_name': 'crm_quotation_report', 'datas': datas, 'nodestroy': True}
+    # Override function that print order
+    def print_quotation(self, cr, uid, ids, context=None):
+        """
+        Note: same at the original except for report_name returned
+        This function prints the sales order and mark it as sent, so that we
+        can see more easily the next step of the workflow
+        """
+        result = super(sale_order_override, self).print_quotation(
+            cr, uid, ids, context=context)
+
+        datas=result.get('datas', {})
+        return {
+            'type': 'ir.actions.report.xml',
+            'report_name': 'crm_quotation_report',
+            'datas': datas,
+            'nodestroy': True}
 
     _columns = {
          'deadline_order': fields.date('Scadenza', required=False, readonly=False, select=True,),
          'footer_annotation_text':fields.char('Annotazione piè pagina', size=64, required=False, readonly=False),
     }
-    
-sale_order_override()        
+
+sale_order_override()
 
 class sale_order_line_override(osv.osv):
-    ''' Change some thing in sale order for use this report
+    """ Change some thing in sale order for use this report
         Add some extra fields for this quotation document
-    '''
+    """
     _name = 'sale.order.line'
     _inherit = 'sale.order.line'
 
     _columns = {
          'footer_annotation':fields.boolean('Nota', required=False, help="Se spuntata mette l'asterisco sul prodotto e attiva la nota a piè pagina"),
     }
-sale_order_line_override()        
+sale_order_line_override()
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
