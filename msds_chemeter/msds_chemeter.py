9d01104cc77b5a062c5227e595e642a62220c603
nicothebrush <nicola.riolini@gmail.com>
2025-02-10 15:33:30 +0100

    generic bugfix (fast message)

--- msds_chemeter/msds_chemeter.py
+++ msds_chemeter/msds_chemeter.py
@@ -224,9 +224,6 @@ class ProductProduct(orm.Model):
             self, cr, uid, ids, field_names, arg=None, context=None):
         """ Extract Mixture MSDS Chemeter for product
         """
-        if uid == 1:
-            pdb.set_trace()
-
         if context is None:
             context = {}
 
