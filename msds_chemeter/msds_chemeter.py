c882c8d140e507a001f1ae7151b518f7ad36c24f
nicothebrush <nicola.riolini@gmail.com>
2025-02-11 14:23:06 +0100

    generic bugfix (fast message)

--- msds_chemeter/msds_chemeter.py
+++ msds_chemeter/msds_chemeter.py
@@ -92,7 +92,7 @@ class MsdsChemeter(orm.Model):
             ctx['sheet_parameter'] = {
                 'mixture': urllib.quote(chemeter.name),
                 'alias': urllib.quote(chemeter.alias),
-                'language': urllib.quote(chemeter.language_id.code)
+                'language': urllib.quote(chemeter.language_id.code),
                 # or 'it-IT'
             }
 
