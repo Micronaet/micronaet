1f48baa052de5436fb3443fb7c51d80a7f990200
nicothebrush <nicola.riolini@gmail.com>
2025-02-27 09:08:20 +0100

    Fast commit

--- mrp_mx_stats_excel/cron/mailer/mrp_stat_report.py
+++ mrp_mx_stats_excel/cron/mailer/mrp_stat_report.py
@@ -311,8 +311,8 @@ else:
     print('[ERR] Connect only SMTP SSL server!')
     sys.exit()
 
-smtp_server.ehlo() #open the connection
-smtp_server.starttls()
+# smtp_server.ehlo() #open the connection
+# smtp_server.starttls()
 smtp_server.login(odoo_mailer.smtp_user, odoo_mailer.smtp_pass)
 
 # Extract 2 files
