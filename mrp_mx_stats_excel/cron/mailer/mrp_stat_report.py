714b453a05ee260b96c8455c1449d01a7863593b
nicothebrush <nicola.riolini@gmail.com>
2025-02-27 09:09:53 +0100

    Fast commit

--- mrp_mx_stats_excel/cron/mailer/mrp_stat_report.py
+++ mrp_mx_stats_excel/cron/mailer/mrp_stat_report.py
@@ -305,7 +305,6 @@ print('[INFO] Sending using "%s" connection [%s:%s]' % (
     odoo_mailer.smtp_port,
     ))
 
-pdb.set_trace()
 if odoo_mailer.smtp_encryption in ('ssl', 'starttls'):
     smtp_server = smtplib.SMTP_SSL(
         odoo_mailer.smtp_host, odoo_mailer.smtp_port)
