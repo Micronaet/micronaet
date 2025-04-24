cbf7da327381ce787b416c747e52645e287c04f8
nicothebrush <nicola.riolini@gmail.com>
2025-04-24 15:41:45 +0200

    Fast commit

--- crm_order_analysis/order.py
+++ crm_order_analysis/order.py
@@ -26,6 +26,8 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+import os
+import sys
 from openerp.osv import osv, fields
 
 
