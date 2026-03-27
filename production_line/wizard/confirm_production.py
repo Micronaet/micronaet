7d853b8e879a563f8b0c6bd1744a703455b0016e
NicoTheBrush <nicola.riolini@gmail.com>
2026-03-27 10:36:54 +0100

    Fast commit

--- production_line/wizard/confirm_production.py
+++ production_line/wizard/confirm_production.py
@@ -533,7 +533,8 @@ class confirm_mrp_production_wizard(osv.osv_memory):
                     _('Cannot write in OpenERP CL number for this load!'),
                 )
                 load_pool.write(cr, uid, load_id, {
-                    'accounting_cl_code': accounting_cl_code}, context=context)
+                    'accounting_cl_code': accounting_cl_code,
+                }, context=context)
 
                 # -------------------------------------------------------------
                 # Update lavoration with new info:
@@ -556,8 +557,7 @@ class confirm_mrp_production_wizard(osv.osv_memory):
                             l.name,
                             partial.product_qty,
                             )
-                cost_detail += \
-                    u'<br/>Totale carichi: <b>%s</b><br/>' % total
+                cost_detail += u'<br/>Totale carichi: <b>%s</b><br/>' % total
 
                 # TODO togliere i commenti nei log e metterli magari nella
                 # lavorazione per sapere come sono stati calcolati
@@ -594,51 +594,43 @@ class confirm_mrp_production_wizard(osv.osv_memory):
                         )
 
                     unload_cost_total = cost_line * total
-                    _logger.info(_('Lavoration %s [%s]') % (
-                        cost_line, unload_cost_total, ))
-                    cost_detail += \
-                        u'<br/>Lavorazione %s: <b>€/kg. %s x %s = %s</b><br/>' % (
-                            cost_line_ref,
-                            cost_line,
-                            total,
-                            unload_cost_total,
-                            )
+                    _logger.info(_('Lavoration %s [%s]') % (cost_line, unload_cost_total, ))
+                    cost_detail += u'<br/>Lavorazione %s: <b>€/kg. %s x %s = %s</b><br/>' % (
+                        cost_line_ref,
+                        cost_line,
+                        total,
+                        unload_cost_total,
+                        )
 
                     # ---------------------------------------------------------
                     # All unload cost of materials (all production):
                     # ---------------------------------------------------------
-                    cost_detail += \
-                        u'<br/><b>Costi materie prime:</b><br/>'
+                    cost_detail += u'<br/><b>Costi materie prime:</b><br/>'
                     cost_detail_subtotal = 0.0
                     for lavoration in mrp.workcenter_lines:
-                        cost_detail += \
-                            u'Lavoratione <b>%s</b>:<br/>' % lavoration.name
+                        cost_detail += u'Lavoratione <b>%s</b>:<br/>' % lavoration.name
                         for unload in lavoration.bom_material_ids:
                             try:
                                 subtotal = unload.product_id.standard_price * unload.quantity
                                 unload_cost_total += subtotal
                                 cost_detail_subtotal += subtotal
 
-                                cost_detail += \
-                                    u' - %s: € %s x q. %s = %s<br/>' % (
-                                        unload.product_id.default_code or '?',
-                                        unload.product_id.standard_price,
-                                        unload.quantity,
-                                        subtotal,
-                                        )
+                                cost_detail += u' - %s: € %s x q. %s = %s<br/>' % (
+                                    unload.product_id.default_code or '?',
+                                    unload.product_id.standard_price,
+                                    unload.quantity,
+                                    subtotal,
+                                    )
                             except:
                                 _logger.error(
                                     _('Error calculating unload lavoration'))
-                    _logger.info(
-                        _('With materials [%s]') % unload_cost_total)
-                    cost_detail += \
-                        u'<b>Totale materie prime: %s</b><br/>' % cost_detail_subtotal
+                    _logger.info(_('With materials [%s]') % unload_cost_total)
+                    cost_detail += u'<b>Totale materie prime: %s</b><br/>' % cost_detail_subtotal
 
                     # ---------------------------------------------------------
                     # All unload package and pallet:
                     # ---------------------------------------------------------
-                    cost_detail += \
-                        u'<br/><b>Costi imballi e pallet:</b><br/>'
+                    cost_detail += u'<br/><b>Costi imballi e pallet:</b><br/>'
                     cost_detail_subtotal = 0.0
                     for l in mrp.workcenter_lines:
                         for load in l.load_ids:
@@ -658,23 +650,22 @@ class confirm_mrp_production_wizard(osv.osv_memory):
                                         link_product.standard_price,
                                         load.ul_qty,
                                     ))
-                                cost_detail += \
-                                    u' - Imballo [%s] %s: € %s x q. %s = %s<br/>' % (
-                                        l.name,
-                                        link_product.default_code or '?',
-                                        link_product.standard_price,
-                                        load.ul_qty,
-                                        subtotal,
-                                        )
+                                cost_detail += u' - Imballo [%s] %s: € %s x q. %s = %s<br/>' % (
+                                    l.name,
+                                    link_product.default_code or '?',
+                                    link_product.standard_price,
+                                    load.ul_qty,
+                                    subtotal,
+                                    )
 
                             except:
                                 _logger.error(
                                     _('Error calculating package price'))
 
                             try:
-                                # ---------------------------------------------
+                                # --------------------------------------------------------------------------------------
                                 # Pallet:
-                                # ---------------------------------------------
+                                # --------------------------------------------------------------------------------------
                                 pallet_in = load.pallet_product_id
                                 if pallet_in: # there's pallet
                                     subtotal = pallet_in.standard_price * load.pallet_qty
@@ -685,21 +676,19 @@ class confirm_mrp_production_wizard(osv.osv_memory):
                                         pallet_in.standard_price,
                                         load.pallet_qty,
                                     ))
-                                    cost_detail += \
-                                        u' - Pallet [%s] %s: € %s x q. %s = %s <br/>' % (
-                                            l.name,
-                                            pallet_in.default_code or '?',
-                                            pallet_in.standard_price,
-                                            load.pallet_qty,
-                                            subtotal,
-                                            )
+                                    cost_detail += u' - Pallet [%s] %s: € %s x q. %s = %s <br/>' % (
+                                        l.name,
+                                        pallet_in.default_code or '?',
+                                        pallet_in.standard_price,
+                                        load.pallet_qty,
+                                        subtotal,
+                                        )
 
                             except:
                                 _logger.error(
                                     _('Error calculating pallet price'))
 
-                    cost_detail += \
-                        u'<b>Totale imballi: %s</b><br/>' % cost_detail_subtotal
+                    cost_detail += u'<b>Totale imballi: %s</b><br/>' % cost_detail_subtotal
 
                     unload_cost = unload_cost_total / total
                     _logger.info(_('With package  %s [unit.: %s]') % (
@@ -707,17 +696,16 @@ class confirm_mrp_production_wizard(osv.osv_memory):
 
                     cost_detail += u'<br/><b>Costo totale:</b><br/>'
                     cost_detail += u'€ %s : q. %s = <b>€/unit %s (carico)</b><br/>' % (
-                            unload_cost_total,
-                            total,
-                            unload_cost,
-                            )
+                        unload_cost_total,
+                        total,
+                        unload_cost,
+                        )
 
-                    # Update all production with value calculated: #TODO serve?
+                    # Update all production with value calculated: # TODO serve?
                     for l in mrp.workcenter_lines:
                         for load in l.load_ids:
                             load_pool.write(cr, uid, load.id, {
-                                'accounting_cost':
-                                    unload_cost * load.product_qty,
+                                'accounting_cost': unload_cost * load.product_qty,
                                 }, context=context)
 
                             # Export CL for update product price:
@@ -762,7 +750,7 @@ class confirm_mrp_production_wizard(osv.osv_memory):
                         if not res_list:
                             raise osv.except_osv(
                                 _('Update CL price error!'),
-                                _('Error launchind update CL price command'),
+                                _('Error launching update CL price command'),
                             )
                         error = (
                             _('Update loaded CL price error!'),
@@ -775,7 +763,7 @@ class confirm_mrp_production_wizard(osv.osv_memory):
                                 _logger.error('No CL: {}'.format(key))
                                 continue
 
-                            if res:  # if True update succesfully
+                            if res:  # if True update successfully
                                 load_pool.write(
                                     cr, uid, load_id,
                                     {'accounting_cost_confirmed': True},
@@ -786,7 +774,7 @@ class confirm_mrp_production_wizard(osv.osv_memory):
                     # ---------------------------------------------------------
                     #            Real workflow operation (only last load)
                     # ---------------------------------------------------------
-                    # TODO Non occorre più verificare togliee questa parte:
+                    # TODO Non occorre più verificare togliere questa parte:
                     # Close production order:
                     # if mrp.accounting_state in ('draft', 'production'):
                     #    if mrp.accounting_state in ('production'):
