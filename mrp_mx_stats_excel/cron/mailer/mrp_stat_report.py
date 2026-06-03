4bdc560a515bacbf5a1e8b21ab94ada733a0c916
NicoTheBrush <nicola.riolini@gmail.com>
2026-06-03 10:40:23 +0200

    Fast commit

--- mrp_mx_stats_excel/cron/mailer/mrp_stat_report.py
+++ mrp_mx_stats_excel/cron/mailer/mrp_stat_report.py
@@ -350,14 +350,12 @@ for mode in smtp['report_mode']:
         msg['To'] = ','.join(smtp['report_mode'][mode]) # XXX See all delivery!
         msg.attach(MIMEText(smtp['text'][mode], 'html'))
 
-        '''
         part = MIMEBase('application', 'octet-stream')
         part.set_payload(open(fullname, 'rb').read())
         Encoders.encode_base64(part)
         part.add_header('Content-Disposition', 'attachment; filename="%s"' % filename)
 
         msg.attach(part)
-        '''
 
         # Send mail:
         smtp_server.sendmail(odoo_mailer.smtp_user, to, msg.as_string())
